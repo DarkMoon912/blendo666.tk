9be034423a05a5cb6b7090d91add65bc8d07b410
DarkMoon912 <86262929+DarkMoon912@users.noreply.github.com>
2022-06-21 23:58:58 +0700

    Бледный хуесос

---
+++ js/config.js
@@ -0,0 +1,44 @@
+let config = {
+
+  // Задержка загрузки (1 секунда)
+  preloaderDelay: 1,
+
+  // Задержка появления видео (4.2 секунды)
+  showDelay: 2.5,
+
+  // Заголовок
+  title: 'Bledno#666',
+
+  // Социальные сети
+  social: {
+    discord:'WnKRSjjUs7',
+    telegram: 'ne666you',
+  },
+
+
+  // Цвет иконки загрузки
+  preloaderColor: '#ff4949',
+
+
+  // Цвет кнопки play
+  playButtonColor: '#ff4949',
+  // Цвет тени кнопки play
+  playButtonShadowColor: '#575757',
+
+
+  // Цвет заголовка
+  titleColor: '#ff4949',
+  // Цвет тени загаловка
+  titleShadowColor: '#575757',
+
+
+  // Текст копирайта
+  copyrightText: 'dev - Bledno666',
+  // Цвет копирайта
+  copyrightTextColor: '#ff4949',
+
+  // Цвет иконок социальных сетей
+  socialColor: '#ff4949',
+  // Цвет тени иконок социальных сетей
+  socialShadowColor: '#575757',
+}
